031676a5fb9819f9d51a955daee27814e6be6876
kvchi <jonathanmkpuma@gmail.com>
2025-04-11 10:15:02 +0100

    Search input adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import {logo} from "../assets/images";
 import { FaSearch } from 'react-icons/fa';
+import { useState } from "react";
 
 export default function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      const targetId = searchInput.trim().replace(/\s+/g, "-").toLowerCase();
+      const targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        targetElement.classList.add("ring-2", "ring-green-400");
+  
+        setTimeout(() => {
+          targetElement.classList.remove("ring-2", "ring-green-400");
+        }, 2000);
+      } else {
+        alert("Item not found");
+      }
+    }
+  };
+  
+
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-black shadow-md fixed w-full top-0 left-0 z-50">
         <div className='flex gap-4 items-center '>
         <img src={logo}  className=" w-[50px] h-[50px] rounded-[50%]" />
-        <h1 className='text-white text-xl font-bold'>Lavish BITES</h1>
+        <h1 className='text-white text-lg md:text-xl font-bold'>Lavish BITES</h1>
         </div>
-        <div className='bg-white p-1 w-30 md:w-50 rounded-2xl'>
-        <FaSearch className="text-gray-600 w-4 h-4" />
-        </div>
+        <div className="flex items-center bg-white px-3 py-2 rounded-2xl max-w-xs shadow-md">
+  <FaSearch className="text-gray-600 w-4 h-4 mr-2" />
+  <input
+    type="text"
+    value={searchInput}
+    onChange={(e) => setSearchInput(e.target.value)}
+    onKeyDown={handleSearch}
+    placeholder="Search menu..."
+    className="outline-none w-full text-sm text-gray-700 placeholder:text-gray-400"
+  />
+</div>
+
     </header>
   )
 }
